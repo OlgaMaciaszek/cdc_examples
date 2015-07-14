c88bcf524a557a21778493561a5f09347e574251
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2015-07-14 16:52:02 +0200

    Modified DSL scripts to apply regexp.

--- fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsFraud.groovy
+++ fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsFraud.groovy
@@ -1,30 +1,36 @@
-io.codearte.accurest.dsl.GroovyDsl.make {
-                request {
-                method """PUT"""
-                url """/fraudcheck"""
-                body('''
-                    {
-                    "clientPesel":"1234567890",
-                    "loanAmount":99999}
-                '''
-                )
-                headers {
-                    header("""Content-Type""", """application/vnd.fraud.v1+json""")
+import groovy.transform.Field
 
-                }
-                
-            }
-            response {
-                status 200
-                body( """{
+@Field
+public static final String PESEL_REGEXP = '[0-9]{11}'
+
+@Field
+public static final String SMALL_AMOUNT_REGEXP = '[1-9]{5}'
+
+io.codearte.accurest.dsl.GroovyDsl.make {
+    request {
+        method """PUT"""
+        url """/fraudcheck"""
+        body(
+                clientPesel: value(client(regex(PESEL_REGEXP)), server("12345678902")),
+                loanAmount: value(client(regex(SMALL_AMOUNT_REGEXP)), server(99999))
+        )
+        headers {
+            header("""Content-Type""", """application/vnd.fraud.v1+json""")
+
+        }
+
+    }
+    response {
+        status 200
+        body("""{
     "fraudCheckStatus": "FRAUD",
     "rejectionReason": "Amount too high"
 }""")
-                headers {
-                     header('Content-Type': 'application/vnd.fraud.v1+json')
+        headers {
+            header('Content-Type': 'application/vnd.fraud.v1+json')
 
-                    }
-                
-            }
-        
+        }
+
+    }
+
 }
--- fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsNotFraud.groovy
+++ fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsNotFraud.groovy
@@ -1,30 +1,33 @@
-io.codearte.accurest.dsl.GroovyDsl.make {
-                request {
-                method """PUT"""
-                url """/fraudcheck"""
-                body('''
-                    {
-                    "clientPesel":"1234567890",
-                    "loanAmount":123.123}
-                '''
-                )
-                headers {
-                    header("""Content-Type""", """application/vnd.fraud.v1+json""")
+import groovy.transform.Field
 
-                }
-                
-            }
-            response {
-                status 200
-                body( """{
+@Field
+public static final String PESEL_REGEXP = '[0-9]{11}'
+
+io.codearte.accurest.dsl.GroovyDsl.make {
+    request {
+        method """PUT"""
+        url """/fraudcheck"""
+        body(
+                clientPesel: value(client(regex(PESEL_REGEXP)), server("12345678902")),
+                loanAmount: 123.123
+        )
+        headers {
+            header("""Content-Type""", """application/vnd.fraud.v1+json""")
+
+        }
+
+    }
+    response {
+        status 200
+        body("""{
     "fraudCheckStatus": "OK",
     "rejectionReason": null
 }""")
-                headers {
-                     header('Content-Type': 'application/vnd.fraud.v1+json')
+        headers {
+            header('Content-Type': 'application/vnd.fraud.v1+json')
 
-                    }
-                
-            }
-        
+        }
+
+    }
+
 }
