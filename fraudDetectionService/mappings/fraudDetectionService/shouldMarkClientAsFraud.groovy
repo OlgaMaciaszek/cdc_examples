169c510c3eab032853a0a59de193842865c45dfb
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2016-02-01 11:10:29 +0100

    Improved dsl syntax.

--- fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsFraud.groovy
+++ fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsFraud.groovy
@@ -7,30 +7,28 @@ public static final String PESEL_REGEXP = '[0-9]{11}'
 public static final String HIGH_AMOUNT_REGEXP = '[1-9]{5}'
 
 io.codearte.accurest.dsl.GroovyDsl.make {
-    request {
-        method """PUT"""
-        url """/fraudcheck"""
-        body(
-                clientPesel: value(stub(regex(PESEL_REGEXP)), test("12345678902")),
-                loanAmount: value(stub(regex(HIGH_AMOUNT_REGEXP)), test(99999))
-        )
-        headers {
-            header("""Content-Type""", """application/vnd.fraud.v1+json""")
+	request {
+		method 'PUT'
+		url '/fraudcheck'
+		body(
+				clientPesel: value(stub(regex(PESEL_REGEXP)), test("12345678902")),
+				loanAmount: value(stub(regex(HIGH_AMOUNT_REGEXP)), test(99999))
+		)
+		headers {
+			header('Content-Type', 'application/vnd.fraud.v1+json')
 
-        }
+		}
 
-    }
-    response {
-        status 200
-        body("""{
-    "fraudCheckStatus": "FRAUD",
-    "rejectionReason": "Amount too high"
-}""")
-        headers {
-            header('Content-Type': 'application/vnd.fraud.v1+json')
+	}
+	response {
+		status 200
+		body(fraudCheckStatus: "FRAUD",
+				rejectionReason: "Amount too high")
+		headers {
+			header('Content-Type': 'application/vnd.fraud.v1+json')
 
-        }
+		}
 
-    }
+	}
 
 }
--- fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsNotFraud.groovy
+++ fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsNotFraud.groovy
@@ -4,30 +4,29 @@ import groovy.transform.Field
 public static final String PESEL_REGEXP = '[0-9]{11}'
 
 io.codearte.accurest.dsl.GroovyDsl.make {
-    request {
-        method """PUT"""
-        url """/fraudcheck"""
-        body(
-                clientPesel: value(client(regex(PESEL_REGEXP)), server("12345678902")),
-                loanAmount: 123.123
-        )
-        headers {
-            header("""Content-Type""", """application/vnd.fraud.v1+json""")
+	request {
+		method 'PUT'
+		url '/fraudcheck'
+		body(
+				clientPesel: value(client(regex(PESEL_REGEXP)), server("12345678902")),
+				loanAmount: 123.123
+		)
+		headers {
+			header('Content-Type', 'application/vnd.fraud.v1+json')
 
-        }
+		}
 
-    }
-    response {
-        status 200
-        body("""{
-    "fraudCheckStatus": "OK",
-    "rejectionReason": null
-}""")
-        headers {
-            header('Content-Type': 'application/vnd.fraud.v1+json')
+	}
+	response {
+		status 200
+		body(
+				"fraudCheckStatus": "OK",
+				"rejectionReason": null)
+		headers {
+			header('Content-Type': 'application/vnd.fraud.v1+json')
 
-        }
+		}
 
-    }
+	}
 
 }
