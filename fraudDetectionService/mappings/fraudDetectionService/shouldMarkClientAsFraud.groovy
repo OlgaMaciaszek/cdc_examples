3ae95e59145385b349c1c0d44f431faa2c0ad75f
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2016-04-07 15:55:54 +0200

    Switched to regex scripts.

--- fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsFraud.groovy
+++ fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsFraud.groovy
@@ -1,10 +1,18 @@
+import groovy.transform.Field
+
+@Field
+public static final String PESEL_REGEXP = '[0-9]{11}'
+
+@Field
+public static final String HIGH_AMOUNT_REGEXP = '[1-9]{5}'
+
 io.codearte.accurest.dsl.GroovyDsl.make {
   request {
     method 'PUT'
     url '/fraudcheck'
     body(
-        clientPesel: "12345678902",
-        loanAmount: 99999
+        clientPesel: value(stub(regex(PESEL_REGEXP)), test("12345678902")),
+        loanAmount: value(stub(regex(HIGH_AMOUNT_REGEXP)), test(99999))
     )
     headers {
       header('Content-Type', 'application/vnd.fraud.v1+json')
@@ -14,8 +22,7 @@ io.codearte.accurest.dsl.GroovyDsl.make {
   }
   response {
     status 200
-    body(
-        fraudCheckStatus: "FRAUD",
+    body(fraudCheckStatus: "FRAUD",
         rejectionReason: "Amount too high")
     headers {
       header('Content-Type': 'application/vnd.fraud.v1+json')
--- fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsNotFraud.groovy
+++ fraudDetectionService/mappings/fraudDetectionService/shouldMarkClientAsNotFraud.groovy
@@ -1,9 +1,14 @@
+import groovy.transform.Field
+
+@Field
+public static final String PESEL_REGEXP = '[0-9]{11}'
+
 io.codearte.accurest.dsl.GroovyDsl.make {
   request {
     method 'PUT'
     url '/fraudcheck'
     body(
-        clientPesel: "12345678902",
+        clientPesel: value(client(regex(PESEL_REGEXP)), server("12345678902")),
         loanAmount: 123.123
     )
     headers {
@@ -14,8 +19,9 @@ io.codearte.accurest.dsl.GroovyDsl.make {
   }
   response {
     status 200
-    body(fraudCheckStatus: "OK",
-        rejectionReason: null)
+    body(
+        "fraudCheckStatus": "OK",
+        "rejectionReason": null)
     headers {
       header('Content-Type': 'application/vnd.fraud.v1+json')
 
