098ca17afbcbd0b30260af04dee3b9102e98acc8
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2016-04-08 11:31:34 +0200

    Raised accurest version. Removed micro-infra setup.

--- fraudDetectionService/src/main/java/com/blogspot/toomuchcoding/frauddetection/Application.java
+++ fraudDetectionService/src/main/java/com/blogspot/toomuchcoding/frauddetection/Application.java
@@ -2,16 +2,12 @@ package com.blogspot.toomuchcoding.frauddetection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.ofg.infrastructure.config.EnableMicroservice;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@EnableMicroservice
 public class Application {
 
 	public static void main(String[] args) {
